70eb0ae259304dd490a1e4a12c02eb3df9336b45
wizi4884 <wizi48wizi@gmail.com>
2022-11-29 18:15:52 +0300

    First Update

---
+++ VoronezhGuide/VoronezhGuide/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace VoronezhGuide;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ VoronezhGuide/VoronezhGuide/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace VoronezhGuide;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
